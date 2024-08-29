1ac59953b13470c828b5c158a777c587d883f029
mdasif8393 <md.asif.8393@gmail.com>
2024-08-29 12:58:22 +0600

    add navigation

--- src/components/Layouts/NavBar.tsx
+++ src/components/Layouts/NavBar.tsx
@@ -1,7 +1,71 @@
+import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="flex items-center justify-center bg-gray-900 h-20">
+      <NavigationMenu>
+        <Link to="/">
+          <div className="flex items-center justify-center mx-2">
+            <img
+              src="https://seeklogo.com/images/C/car-wash-logo-DDA5359527-seeklogo.com.png"
+              alt=""
+              className="w-14"
+            />
+            <h4 className="text-xl font-bold text-white ml-2">Car Wash 24</h4>
+          </div>
+        </Link>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/">Home</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/products">Products</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/about">About US</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/product-management">Product Management</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/checkout">Checkout</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        <Link to="/cart">
+          <div className="h-10 w-12 rounded  bg-gray-100 flex justify-center items-center hover:bg-slate-300">
+            <div className="relative py-2">
+              <div className="t-0 absolute left-3">
+                <p className="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
+                  {/* {products?.length} */}
+                </p>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="file: mt-4 h-6 w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </div>
+          </div>
+        </Link>
+      </NavigationMenu>
     </div>
   );
 };
