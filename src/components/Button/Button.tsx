195bb342d8d0be4df312269edb7e373d80c5874e
mdasif8393 <md.asif.8393@gmail.com>
2024-08-31 16:36:51 +0600

    add sign in

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const Button = ({ text }: any) => {
   return (
-    <button className="relative px-5 py-2 font-medium text-white group w-40 h-11">
+    <button className="relative px-5 py-2 font-medium text-white group w-48 h-11">
       <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-gray-500 group-hover:bg-gray-700 group-hover:skew-x-12"></span>
       <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-gray-700 group-hover:bg-gray-500 group-hover:-skew-x-12"></span>
       <span className="absolute bottom-0 left-0 hidden w-14 h-28 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-gray-600 -rotate-12"></span>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,10 @@
+import SignIn from "@/components/Authentication/SignIn";
+import SignUp from "@/components/Authentication/SignUp";
 import Bookings from "@/components/Bookings/Bookings";
 import Home from "@/components/Home/Home";
 import MainLayout from "@/components/Layouts/MainLayout";
 import NotFound from "@/components/NotFound/NotFound";
 import Services from "@/components/Services/Services";
-import SignIn from "@/components/SignIn/SignIn";
-import SignUp from "@/components/SignUp/SignUp";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
